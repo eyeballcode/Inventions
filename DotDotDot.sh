2c3077ff418feceff06c232017a0d5f017f64c77
Eyeball Edward <edwardyeung.private@gmail.com>
2014-09-13 09:58:07 +0800

    Added DotDotDot.sh

---
+++ DotDotDot.sh
@@ -0,0 +1,16 @@
+echo "Press ^C to quit"
+sleep 1
+clear
+while true
+do
+  echo -n "."
+  sleep 0.5
+  echo -n "."
+  sleep 0.5
+  echo -n "."
+  sleep 0.5
+  clear
+  clear
+  clear
+  clear
+done;
