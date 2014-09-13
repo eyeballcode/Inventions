1599cbd54b5ebef051aaa6fe25f2e1bee3442253
Eyeball Edward <edwardyeung.private@gmail.com>
2014-09-13 09:43:13 +0800

    first commit

---
+++ Rotate.sh
@@ -0,0 +1,18 @@
+echo  "Press ^C to quit."
+sleep 1
+while true
+do
+  echo -n "-"
+  sleep 0.1
+
+  echo -n "\\"
+  sleep 0.1
+
+  echo -n "|"
+  sleep 0.1
+
+  echo -n "/"
+  sleep 0.1
+
+  echo -n ""
+done;
